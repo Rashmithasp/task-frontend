32df567311b218a4001e84ddd85175b63b40dbcc
Rashmithasp <rashmithasp85@gmail.com>
2025-08-22 15:24:20 +0530

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,51 @@
-import { useState,useEffect, use } from "react";
+import { useState,useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [task,setTasks]=useState([]);
-  const [title,setTitle]=useState("");
-
+  const [task,setTasks] = useState([]);
+  const [title,setTitle] = useState("");
   const API_URL = import.meta.env.VITE_API_URL;
 
-  const getTasks=async()=>{
-    const res=await axios.get(`${API_URL}/api/tasks`);
+  const getTasks = async()=> {
+    const res = await axios.get(`${API_URL}/api/tasks`);
     setTasks(res.data);
-    
+
   };
-  const createTask=async()=>{
-    if(!title) return;
+  const createTask = async()=> {
+    if (!title) return;
     await axios.post(`${API_URL}/api/tasks`,{title});
     setTitle("");
     getTasks();
+  
   };
-  const deleteTask=async(id)=>{
+  const deleteTask = async(id) => {
     await axios.delete(`${API_URL}/api/tasks/${id}`);
     getTasks();
-  };
-
-  useEffect(()=>{
+  }
+  useEffect(() => {
     getTasks();
   },[]);
-  return (
+  return(
     <div>
-      <h2>Task manager</h2>
+      <h2>TASK MANAGER</h2>
       <input 
       type="text"
-      value={title}
-      placeholder="Enter task title"
+      value = {title} 
+      placeholder="ENTER THE TASK"
       onChange={(e)=>setTitle(e.target.value)}
       />
-      <button onClick={createTask}>Add</button>
+      <button onClick={createTask}> ADD</button>
       <ul>
-        {task.map((task)=>{
-          <li key={task_id}>
-            {task.title}
-            <button onClick={()=>deleteTask(task._id)}>Delete</button>
-          </li>
-
-        })}
-      </ul>
-          
+  {Array.isArray(task) && task.map((task) => (
+    <li key={task._id}>
+      {task.title}
+      <button onClick={() => deleteTask(task._id)}> DELETE</button>
+    </li>
+    ))}
+     </ul>
     </div>
   );
+
 }
 export default App;
-
